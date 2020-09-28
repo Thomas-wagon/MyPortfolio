98a63d107d2ec779b7302233f98194742fd0ba21
thomas viaules <thomas.viaules@gmail.com>
2020-09-28 14:39:13 -0400

    parallax effect on banner done

--- src/components/banner_2.js
+++ src/components/banner_2.js
@@ -1,10 +1,16 @@
 import React from "react"
 import "./banner_2.scss"
-import Layer1 from "../images/layer_1.png"
-import Layer2 from "../images/layer_2.png"
-import Layer3 from "../images/layer_3.png"
-import Layer4 from "../images/layer_4.png"
-import Layer5 from "../images/layer_5.png"
+import Layer1 from "../images/large_layer_1.png"
+import Layer2 from "../images/large_layer_2.png"
+import Layer3 from "../images/large_layer_3.png"
+import Layer4 from "../images/large_layer_4.png"
+import Layer5 from "../images/large_layer_5.png"
+
+// import Layer1 from "../images/layer_1.png"
+// import Layer2 from "../images/layer_2.png"
+// import Layer3 from "../images/layer_3.png"
+// import Layer4 from "../images/layer_4.png"
+// import Layer5 from "../images/layer_5.png"
 
 export default function Banner() {
     return (
@@ -18,13 +24,13 @@ export default function Banner() {
             <div className="parallax_layer parallax_layer_3">
                 <img src={Layer3} alt="parallax layer 3"/>
             </div>
-            <div className="parallax_layer parallax_birds">
-                <img src={Layer4} alt="parallax birds"/>
-            </div>
             <div className="parallax_layer parallax_layer_4">
-                <img src={Layer5} alt="parallax layer 4"/>
+                <img src={Layer4} alt="parallax layer 4"/>
             </div>
-            <div class="parallax__cover"></div>
+            <div className="parallax_layer parallax_layer_5">
+                <img src={Layer5} alt="parallax layer 5"/>
+            </div>
+            <div class="parallax_cover"></div>
         </div>
     )
 }
